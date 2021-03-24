38407fd648303b2e01a14b36d53a9493333f4623
Ângelo Salles <angelo_848@live.com>
2021-03-23 22:09:56 -0300

    envio de mensagem finalizado

--- src/controllers/whatsapp.js
+++ src/controllers/whatsapp.js
@@ -1,27 +1,53 @@
-const puppeteer = require('puppeteer')
-const util = require('../utils/utils')
-const fs = require('fs')
+'use strict'
 
+const fs = require('fs')
+const tokens = require('../../auth.json')
+const puppeteer = require('puppeteer')
 module.exports = {
   async sendMessage(req, res) {
     try {
       const browser = await puppeteer.launch({ headless: false })
       const page = await browser.newPage()
+      page.on('dialog', async dialog => {
+        await dialog.accept()
+      })
+
+      await page.goto('https://web.whatsapp.com')
+      await page.evaluate((tokens) => {
+        localStorage.clear();
+        localStorage.setItem('WAToken1', JSON.stringify(tokens.WAToken1))
+        localStorage.setItem('WAToken2', JSON.stringify(tokens.WAToken2))
+        localStorage.setItem('WABrowserId', JSON.stringify(tokens.WABrowserId))
+        localStorage.setItem('WASecretBundle', JSON.stringify({
+          key: tokens.WASecretBundle.key,
+          encKey: tokens.WASecretBundle.encKey,
+          macKey: tokens.WASecretBundle.macKey
+        }))
+      }, tokens)
 
       const { phones } = req.body
 
-      for (const phone of phones) {
-
-        await page.goto(`https://web.whatsapp.com/send?phone=${phone}&text=${req.body.message}`);
-
-        const frame = page.mainFrame()
-
-        const btnSend = await frame.waitForSelector('._2Ujuu')
-        await btnSend.click()
-        await util.sleep(2000)
+      const counter = {
+        sucess: [],
+        fails: []
       }
 
-      res.send('ok')
+      for (const phone of phones) {
+        try {
+          await page.goto(`https://web.whatsapp.com/send?phone=${phone}&text=${req.body.message}`);
+          await page.waitForSelector("div#startup", { hidden: true, timeout: 60000 });
+          const btnSend = await page.waitForSelector('._1E0Oz')
+          await btnSend.click()
+          await page.waitForTimeout(500)
+          counter.sucess.push(phone)
+        } catch (error) {
+          counter.fails.push(phone)
+          console.log(error)
+        }
+      }
+
+      res.send(counter)
+      await browser.close()
     } catch (error) {
       console.log(error)
       res.status(500).json(error)
@@ -42,10 +68,10 @@ module.exports = {
 
     await page.waitForSelector('.two')
     const tokens = await page.evaluate(() => {
-      const WAToken1 = localStorage.getItem('WAToken1')
-      const WAToken2 = localStorage.getItem('WAToken2')
-      const WABrowserId = localStorage.getItem('WABrowserId')
-      const WASecretBundle = localStorage.getItem('WASecretBundle')
+      const WAToken1 = JSON.parse(localStorage.getItem('WAToken1'))
+      const WAToken2 = JSON.parse(localStorage.getItem('WAToken2'))
+      const WABrowserId = JSON.parse(localStorage.getItem('WABrowserId'))
+      const WASecretBundle = JSON.parse(localStorage.getItem('WASecretBundle'))
 
       return {
         WAToken1,
@@ -55,7 +81,6 @@ module.exports = {
       }
     })
 
-    console.log(typeof JSON.stringify(tokens))
-    fs.writeFileSync('./../auth.json', JSON.stringify(tokens))
+    fs.writeFileSync('auth.json', JSON.stringify(tokens))
   }
 }
--- src/utils/utils.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  async sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-  }
-
-}
